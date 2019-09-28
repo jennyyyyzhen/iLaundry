9292116be27cdbe26bb67a646e7bd9ed8940ae88
jennyyyyzhen <yzhen@cs.hmc.edu>
2019-09-27 20:12:04 -0700

    built a basic version of an android app

---
+++ myfirstapp/app/src/main/java/com/example/myfirstapp/EastDorm.java
@@ -0,0 +1,14 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EastDorm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_east_norm);
+    }
+}
