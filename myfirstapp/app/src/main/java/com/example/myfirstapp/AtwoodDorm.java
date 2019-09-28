14a75a18ae5f9ef239908991dd296b7b129fad99
jennyyyyzhen <yzhen@cs.hmc.edu>
2019-09-27 23:02:39 -0700

    added function to set database value

--- myfirstapp/app/src/main/java/com/example/myfirstapp/AtwoodDorm.java
+++ myfirstapp/app/src/main/java/com/example/myfirstapp/AtwoodDorm.java
@@ -3,6 +3,10 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AtwoodDorm extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class AtwoodDorm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atwood_dorm);
+    }
+    public void changeStatus(View view){
+        DatabaseReference database=FirebaseDatabase.getInstance().getReference();
+        database.child("north").child("dryer").child("dryer1").setValue(true);
+
     }
 }
