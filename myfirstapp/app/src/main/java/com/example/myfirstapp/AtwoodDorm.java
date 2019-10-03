e2fca0db4608d01bf4232eb124dfb700153e9568
jennyyyyzhen <yzhen@cs.hmc.edu>
2019-10-02 22:01:08 -0700

    Add a countdown timer to a button(test)

--- myfirstapp/app/src/main/java/com/example/myfirstapp/AtwoodDorm.java
+++ myfirstapp/app/src/main/java/com/example/myfirstapp/AtwoodDorm.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,6 +18,8 @@ import com.google.firebase.database.ValueEventListener;
 
 public class AtwoodDorm extends AppCompatActivity {
 
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
     @Override
     /*
         Display the layout when the new activity is created.
@@ -22,14 +27,32 @@ public class AtwoodDorm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atwood_dorm);
+
+        final Button button = (Button)  findViewById(R.id.Atwood_dryer_2);
+        final DatabaseReference NODE = database.child("Atwood").child("dryer").child("2");
+
+        NODE.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Boolean status = dataSnapshot.getValue(Boolean.class);
+                button.setText(Boolean.toString(status));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
-    /* This function is called when a button is clicked. It gets the ID of that button and
+
+    /*
+    This function is called when a button is clicked. It gets the ID of that button and
         the check the corresponding value stored in the database. If the status is currently
         true, then changed it to false. Otherwise, change it to true
      */
-    public void changeStatus(View view){
-        DatabaseReference database=FirebaseDatabase.getInstance().getReference();
+    public void changeStatus(View view) {
 
         int viewID = view.getId();
         String viewName = getResources().getResourceName(viewID);
@@ -39,19 +62,29 @@ public class AtwoodDorm extends AppCompatActivity {
         String machine = childNodes[1];
         String num = childNodes[2];
 
-        final DatabaseReference NODE = database.child(dorm).child(machine).child(num);
+        final Button button = (Button) view;
+        StringBuilder sb = new StringBuilder(button.getText());
+        String status = sb.toString();
 
-        NODE.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Boolean status = dataSnapshot.getValue(Boolean.class);
-                NODE.setValue(!status);
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+        final DatabaseReference node = database.child(dorm).child(machine).child(num);
 
-            }
-        });
 
+        if (status.equals("true")) {
+            Log.e(status,"status");
+            CountDownTimer countDownTimer = new CountDownTimer(10 * 1000, 1000) {
+                @Override
+                public void onTick(long l) {
+                    button.setText("seconds" + l);
+                    node.setValue(false);
+                }
+
+                public void onFinish() {
+                    button.setText("true");
+                    node.setValue(true);
+                }
+            }.start();
+        }
     }
 }
+
+
--- myfirstapp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ myfirstapp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -29,48 +29,48 @@ public class MainActivity extends AppCompatActivity {
      users. It would start a new activity and navigate user to the new page.
      */
     public void goEastDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the East Dorm button
         Intent intent = new Intent(this, EastDorm.class);
         startActivity(intent);
     }
 
     public void goWestDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the West Dorm button
         Intent intent = new Intent(this, WestDorm.class);
         startActivity(intent);
     }
     public void goSouthDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the South Dorm button
         Intent intent = new Intent(this, SouthDorm.class);
         startActivity(intent);
     }
     public void goNorthDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the North Dorm button
         Intent intent = new Intent(this, NorthDorm.class);
         startActivity(intent);
     }
     public void goSontagDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the Sontag Dorm button
         Intent intent = new Intent(this, SontagDorm.class);
         startActivity(intent);
     }
     public void goCaseDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the Case Dorm button
         Intent intent = new Intent(this, CaseDorm.class);
         startActivity(intent);
     }
     public void goDrinkwardDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the Drinkward Dorm button
         Intent intent = new Intent(this, DrinkwardDorm.class);
         startActivity(intent);
     }
     public void goLindeDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the Linde Dorm button
         Intent intent = new Intent(this, LindeDorm.class);
         startActivity(intent);
     }
     public void goAtwoodDorm(View view){
-        //called when the user taps the send button
+        //called when the user taps the Atwood Dorm button
         Intent intent = new Intent(this, AtwoodDorm.class);
         startActivity(intent);
     }
