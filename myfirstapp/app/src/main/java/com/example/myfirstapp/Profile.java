ea0e0d8b92f90bb77d3060752bb4bbc5e2fa342f
jennyyyyzhen <yzhen@cs.hmc.edu>
2019-11-18 15:57:03 -0800

    enable users to change username

--- myfirstapp/app/src/main/java/com/example/myfirstapp/Profile.java
+++ myfirstapp/app/src/main/java/com/example/myfirstapp/Profile.java
@@ -1,12 +1,22 @@
 package com.example.myfirstapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,20 +30,64 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        setNameDisplay();
+        setDisplay();
     }
 
-    private void setNameDisplay() {
+    private void setDisplay() {
         FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = auth.getCurrentUser();
-        String uid = firebaseUser.getUid();
+
         String name = firebaseUser.getDisplayName();
+        String emailAddress =  firebaseUser.getEmail();
+
 
         // find the corresponding button and node in the database
-        int resID = getResources().getIdentifier("user_name", "id", getPackageName());
-        final TextView status = (TextView) findViewById(resID);
+        int userNameID = getResources().getIdentifier("user_name", "id", getPackageName());
+        final TextView userName= (TextView) findViewById(userNameID);
 
-        status.setText("User Name: " + name + "\n" + "UID: " + uid);
+        // find the corresponding button and node in the database
+        int userEmailID = getResources().getIdentifier("user_email", "id", getPackageName());
+        final TextView userEmail = (TextView) findViewById(userEmailID);
+
+        userName.setText("Username: " + name + "\n");
+        userEmail.setText("User Email: " + emailAddress +"\n");
+
+    }
+
+    public void showInputDialog(View view) {
+        /*@setView 装入一个EditView
+         */
+        final EditText editText = new EditText(Profile.this);
+        AlertDialog.Builder inputDialog =
+                new AlertDialog.Builder(Profile.this);
+        inputDialog.setTitle("New Username").setView(editText);
+        inputDialog.setPositiveButton("Confirm",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String username = editText.getText().toString();
+                        updateUsername(username);
+
+                    }
+                }).show();
+    }
+
+    private void updateUsername(String username){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setDisplayName(username)
+                .build();
+
+        user.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Profile.this, "Username changed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 
